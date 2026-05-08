2396384bf004baf15fd26dd7cb8d720fdb3263ff
Doyen Auto <info@doyenautos.co.uk>
2026-05-08 23:36:13 +0000

    fix: add unix socket server.js for shared hosting deployment

---
+++ server.js
@@ -0,0 +1,27 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+const path = require("path");
+
+const SOCKET = "/tmp/rave_ravesoft.sock";
+const dir = path.join(__dirname);
+const app = next({ dev: false, dir });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  // Remove stale socket file
+  if (fs.existsSync(SOCKET)) {
+    fs.unlinkSync(SOCKET);
+  }
+
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(SOCKET, (err) => {
+    if (err) throw err;
+    // Allow Apache/nginx to read the socket
+    fs.chmodSync(SOCKET, "777");
+    console.log("> RaveSoft ready on unix socket:", SOCKET);
+  });
+});
